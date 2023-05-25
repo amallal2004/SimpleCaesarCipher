90fac6f6cb8ddb2d12699af20985284c3e77e912
Amal Lal <110350036+amallal2004@users.noreply.github.com>
2023-05-25 22:24:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import art  # Import the art module for logo display
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u',
@@ -17,15 +16,14 @@ def cipher(start_text, key, cipher_direction):
     new_letter = ''
     end_text = ""
     for letter in start_text:
-        position = alphabet.index(letter)
         if key > 26:
             key %= 26
         if letter not in alphabet:
-            end_text += letter
+            new_letter = letter
         elif cipher_direction == 'encode':
-            new_letter = alphabet[position + key]
+            new_letter = alphabet[alphabet.index(letter) + key]
         elif cipher_direction == 'decode':
-            new_letter = alphabet[position - key]
+            new_letter = alphabet[alphabet.index(letter) - key]
         end_text += new_letter
     print(f"The {cipher_direction}d text is {end_text}")
 
